098ad8eaa1d9210850189dc5363ded11e9705fe8
Johannes Rudolph <johannes.rudolph@gmx.com>
2017-12-17 16:18:26 +0100

    Add ENV.samle-h

---
+++ ENV.sample.h
@@ -0,0 +1,6 @@
+const char* ssid = "WIFISSID";
+const char* password = "WIFIPass";  
+
+IPAddress ip(192,168,1,6);  //Node static IP
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
